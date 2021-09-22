9983d452c360643203beb20f6039675861efe9a4
saurabhsathe <sathesaurabh30@gmail.com>
2021-09-22 23:55:42 +0530

    added Dashboard with navigation bar and filters at the top, progress on dashboard is 60%

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import Sidepanel from './SidePanel'
+import BootCdn from './BootCdn'
+import SignupForm from './SignupForm'
+import LoginForm from './LoginForm'//Create a Main Component
+/*
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                {/*Render Different Component based on Route}
+                <Route path="/" component={Navbar}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/home" component={Home}/>
+                <Route path="/delete" component={Delete}/>
+                <Route path="/create" component={Create}/>
+            </div>
+        )
+    }
+}
+//Export The Main Component
+export default Main;
+*/
