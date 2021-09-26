2e64a56b44a1499f8067c32e20da4971b8cbab12
saurabhsathe <sathesaurabh30@gmail.com>
2021-09-26 00:29:09 -0700

    added states to login and signup forms of resteraunt

--- frontend/src/components/login_and_signup/RestoSignup.js
+++ frontend/src/components/login_and_signup/RestoSignup.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 const RestoSignup = () => {
+    const [oname,setoname] = useState();
+    const [oaddr,setoaddr] = useState();
+    const [ozip,setozip] = useState();
+    const [ocontact,setocontact] = useState();
+    const [opwd,setopwd] = useState();
+    const [oemail,setoemail] = useState();
+    const [ocpwd,setocpwd] = useState();
+    const [restdp,setrestdp] = useState();
+    const [restname,setrestname] = useState();
+    const [restaddr,setrestaddr] = useState();
+    const [restzip,setrestzip] = useState();
+    
+    
+    
+    
+    
     function handleRegister(e){
         console.log("in the register register resteraunt")
     }
@@ -13,37 +29,37 @@ const RestoSignup = () => {
                <form onSubmit={handleRegister}>
                   <div className="form-group">
                      <label>Owner Name</label>
-                     <input type="text" id="oname" className="form-control" placeholder="Your Name" required/>
+                     <input type="text" id="oname" className="form-control" placeholder="Your Name" value={oname} onChange={e=>setoname(e.target.value)} required/>
                   </div>
                   <div className="form-group">
                      <label>Owner Address</label>
-                     <input type="text" id="oaddr" className="form-control" placeholder="Address" required/>
+                     <input type="text" id="oaddr" className="form-control" value={oaddr} onChange={e=>setoaddr(e.target.value)} placeholder="Address" required/>
                   </div>
                   <div className="form-group">
                      <label>Owner Zipcode</label>
-                     <input type="text" pattern="[0-9]{5}" id="ozip" className="form-control" placeholder="Address" required/>
+                     <input type="text" pattern="[0-9]{5}" id="ozip" className="form-control" value={ozip} onChange={e=>setozip(e.target.value)} placeholder="Address" required/>
                   </div>
          
 
 
                   <div className="form-group">
                      <label>Owner Contact No.</label>
-                     <input type="tel" id="ocontact" className="form-control" placeholder="Your contact number please" required/>
+                     <input type="tel" id="ocontact" className="form-control" value={ocontact} onChange={e=>setocontact(e.target.value)} placeholder="Your contact number please" required/>
                   </div>
          
                   <div className="form-group">
                      <label>Owner Email</label>
-                     <input type="email" id="oemail" className="form-control" placeholder="Your email address" required/>
+                     <input type="email" id="oemail" className="form-control" value={oemail} onChange={e=>setoemail(e.target.value)} placeholder="Your email address" required/>
                   </div>
                   
                   <div className="form-group">
                      <label>Password</label>
-                     <input type="password" id="opwd" className="form-control" placeholder="Password" required/>
+                     <input type="password" id="opwd" className="form-control" value={opwd} onChange={e=>setopwd(e.target.value)} placeholder="Password" required/>
                   </div>
 
                 <div className="form-group">
                      <label>Confirm Password</label>
-                     <input type="password" id="ucpwd" className="form-control" placeholder="Password" required/>
+                     <input type="password" id="ocpwd" className="form-control" value={ocpwd} onChange={e=>setocpwd(e.target.value)} placeholder="Password" required/>
                   </div>
                 <hr />
                 <h2><b>Resteraunt details</b></h2>
@@ -51,18 +67,21 @@ const RestoSignup = () => {
 
                 <div className="form-group">
                      <label>Resteraunt Name</label>
-                     <input type="text" id="restname" className="form-control" placeholder="Your Name" required/>
+                     <input type="text" id="restname" className="form-control" value={restname} onChange={e=>setrestname(e.target.value)} placeholder="Your Name" required/>
                   </div>
                   <div className="form-group">
                      <label>Resteraunt Address</label>
-                     <input type="text" id="restaddr" className="form-control" placeholder="Address" required/>
+                     <input type="text" id="restaddr" className="form-control" value={restaddr} onChange={e=>setrestaddr(e.target.value)} placeholder="Address" required/>
                   </div>
                   <div className="form-group">
                      <label>Resteraunt Zipcode</label>
-                     <input type="text" pattern="[0-9]{5}" id="restzip" className="form-control" placeholder="Zipcode" required/>
+                     <input type="text" pattern="[0-9]{5}" id="restzip" className="form-control" value={restzip} onChange={e=>setrestzip(e.target.value)} placeholder="Zipcode" required/>
                   </div>
          
-
+                  <div className="form-group">
+                     <label>Upload Resteraunt Picture</label>
+                     <input type="file" id="restdp" name="restdp" value={restdp} onChange={e => setrestdp(e.target.value)}  accept="image/x-png,image/gif,image/jpeg" />
+                  </div>
 
                   
                     
--- frontend/src/components/login_and_signup/SignupForm.js
+++ frontend/src/components/login_and_signup/SignupForm.js
@@ -2,13 +2,13 @@ import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 const SignupForm = () => {
     const [uname,setuname] = useState();
-    const [uaddr,setaddr] = useState();
-    const [uzip,setzip] = useState();
-    const [ucontact,setcontact] = useState();
+    const [uaddr,setuaddr] = useState();
+    const [uzip,setuzip] = useState();
+    const [ucontact,setucontact] = useState();
     const [upwd,setupwd] = useState();
-    const [uemail,setemail] = useState();
+    const [uemail,setuemail] = useState();
     const [ucpwd,setucpwd] = useState();
-    const [udp,setdp] = useState();
+    const [udp,setudp] = useState();
     function handleRegister(e){
         console.log("in the register user")
     }
@@ -22,42 +22,42 @@ const SignupForm = () => {
                <form onSubmit={handleRegister}>
                   <div className="form-group">
                      <label>Name</label>
-                     <input type="text" value={uname} id="uname" className="form-control" placeholder="Your Name" required/>
+                     <input type="text" value={uname} id="uname" className="form-control" placeholder="Your Name" onChange={e => setuname(e.target.value)} required/>
                   </div>
                   <div className="form-group">
                      <label>Address</label>
-                     <input type="text" value={uaddr} id="uaddr" className="form-control" placeholder="Address" required/>
+                     <input type="text" value={uaddr} id="uaddr" className="form-control" placeholder="Address" onChange={e => setuaddr(e.target.value)} required/>
                   </div>
                   <div className="form-group">
                      <label>Zipcode</label>
-                     <input type="text" value={uzip} pattern="[0-9]{5}" id="uzip" className="form-control" placeholder="Address" required/>
+                     <input type="text" value={uzip} pattern="[0-9]{5}" id="uzip" className="form-control" placeholder="Address" onChange={e => setuzip(e.target.value)} required/>
                   </div>
          
 
 
                   <div className="form-group">
                      <label>Contact No.</label>
-                     <input type="tel" id="ucontact" value={ucontact} className="form-control" placeholder="Your contact number please" required/>
+                     <input type="tel" id="ucontact" value={ucontact} className="form-control" placeholder="Your contact number please" onChange={e => setucontact(e.target.value)} required/>
                   </div>
          
                   <div className="form-group">
                      <label>Email</label>
-                     <input type="email" id="uemail" value={uemail} className="form-control" placeholder="Your email address" required/>
+                     <input type="email" id="uemail" value={uemail} className="form-control" placeholder="Your email address" onChange={e => setuemail(e.target.value)} required/>
                   </div>
                   
                   <div className="form-group">
                      <label>Password</label>
-                     <input type="password" id="upwd" value={upwd} className="form-control" placeholder="Password" required/>
+                     <input type="password" id="upwd" value={upwd} className="form-control" onChange={e => setupwd(e.target.value)} placeholder="Password" required/>
                   </div>
 
                 <div className="form-group">
                      <label>Confirm Password</label>
-                     <input type="password" id="ucpwd" value={ucpwd} className="form-control" placeholder="Password" required/>
+                     <input type="password" id="ucpwd" value={ucpwd} className="form-control" onChange={e => setucpwd(e.target.value)} placeholder="Password" required/>
                   </div>
                 
                   <div className="form-group">
                      <label>Upload Profile Picture</label>
-                     <input type="file" id="dp" name="dp" value={udp} accept="image/x-png,image/gif,image/jpeg" />
+                     <input type="file" id="dp" name="dp" value={udp} onChange={e => setudp(e.target.value)} accept="image/x-png,image/gif,image/jpeg" />
                   </div>
 
 
