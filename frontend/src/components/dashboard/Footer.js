5796bc7927a67925507314f5b08d2ca33ce9f8fa
Saurabh Sathe <sathesaurabh30@gmail.com>
2021-10-10 19:28:14 -0700

    Suddenly the name of the folder changed

---
+++ frontend/src/components/dashboard/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+    return (
+       
+            <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          
+
+          
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://ubereats.com/about/">About Us</a></li>
+              <li><a href="http://ubereats.com/contact/">Contact Us</a></li>
+              <li><a href="http://ubereats.com/contribute-at-ubereats/">Contribute</a></li>
+              <li><a href="http://ubereats.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://ubereats.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+   
+        <hr />
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; All Rights Reserved by 
+         <a href="#">Uber Eats</a>.
+            </p>
+          </div>
+
+         
+        </div>
+      </div>
+    </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/dashboard/Resteraunts.js
@@ -0,0 +1,104 @@
+import React, {Component}  from 'react'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import Jumbo from './Jumbo'
+import DashNavbar from './DashNavbar'
+
+import { useCookies } from "react-cookie";
+import RestoCard from './RestoCard'
+
+const Resteraunts = (props) => {
+    const [original_restos,setoriginal_restos]=useState([])
+    let [restos_received,setrestos]=useState([])
+    const [cookies, setCookie] = useCookies(["restaurant"]);
+
+    function filter_restos(){
+
+    }
+
+    useEffect(()=>{
+        if(Object.keys(props).length == 0){
+        console.log("herhehrehrhehrehrehh")
+             var headers = new Headers(); 
+           const data = {
+               resteraunt_name:"dasdsadsa"
+           }
+  
+        axios.post("http://54.176.82.69:3001/getallResto",data).then(response=>{
+                
+                if(response.status === 200)
+                {
+                    
+                    console.log(response.data,typeof response.data)
+                    setoriginal_restos(response.data[0])
+                    setrestos(response.data[0])
+                    console.log("got the restaurants",restos_received)
+                    
+                }
+                else if(response.status === 202)
+                {
+                    console.log("no data found")
+                }
+
+        })
+       
+    }
+    else{
+        console.log("got some props");
+    }
+
+
+
+},[]);
+  
+
+let details_received= restos_received.map((resto,index) => {
+    return(
+     
+    <RestoCard
+    resto={resto}  
+    restdp={resto.restdp} 
+    resteraunt_name ={resto.resteraunt_name}
+    address={resto.address}
+    zipcode={resto.zipcode}
+
+    />
+
+    )
+})
+
+
+    return (
+
+
+            <div>
+
+                 <div className="landing">
+            <DashNavbar filter={filter_restos} orestos={original_restos} setrestos={setrestos} />
+            <Jumbo />
+            
+
+
+        </div>
+            <div id="services" className="container">
+            
+            
+   <h2 className="display-4 text-center mt-5 mb-3">Restaurants</h2>
+        
+   <div className="row text-center">
+      {details_received}
+     
+
+
+
+    </div>
+    
+
+
+</div>
+</div>
+
+    )
+}
+
+export default Resteraunts
