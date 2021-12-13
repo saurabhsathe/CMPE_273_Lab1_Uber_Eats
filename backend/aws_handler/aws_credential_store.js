d453a3e9ec341e6c3168249272ff121bc5064d79
Saurabh Sathe <sathesaurabh30@gmail.com>
2021-12-13 01:52:54 -0800

    Update aws_credential_store.js

--- backend/aws_handler/aws_credential_store.js
+++ backend/aws_handler/aws_credential_store.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 function getS3(){
     return new AWS.S3({
-  accessKeyId: 'AKIARXHLWFULHJKSQKMK',
-  secretAccessKey: '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'
+  accessKeyId: '',
+  secretAccessKey: ''
 });
 }
 module.exports={getS3}
